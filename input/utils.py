170ba04bb7c92b60326f34a83993e875244f9c6f
Carles Carmona Calpe <altran-services.carles.carmona.calpe@hp.com>
2017-11-10 16:04:05 +0100

    Improvements ------------   - 2n version of the Notebook   - Updated train, test, solution dataset   - Simple method to compute the accuracy

---
+++ input/utils.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def accuracy_score(dataset_input):
+	"""
+	Lazy way to compute the accuracy of the dataset, the column survived 
+	has to exist in the input DataFrame and the order has to be
+	the same
+
+	Parameters
+	-----------
+	dataset_input : Dataframe
+		Dataset to verify
+	"""
+
+	# Requires a colum called survived
+	assert 'survived' in dataset_input.columns,\
+	"The input dataset for compute the accuracy requires one column named 'survived'"
+
+	# Load the dataset
+	sol = pd.read_csv("../input/sol.csv", index_col='id')
+
+	# Compute the accuracy
+	return np.sum(sol["survived"] == dataset_input["survived"]) / float(sol.shape[0]) 
+
+
+if __name__ == "__main__":
+	test = pd.read_csv("../input/sol.csv")
+	print accuracy_score(test)
